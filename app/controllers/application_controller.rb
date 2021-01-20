e8e33d66f2c69e0bc63db3a96e97a59e26d1ef4e
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-20 19:37:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  post '/recipes' do
-    erb :new
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
   end
 
 end
